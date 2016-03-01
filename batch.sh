244feded710fd8f75f82e0a775ef2c6efad35c2f
Timo Bingmann <tbgit@panthema.net>
2016-03-01 20:35:26 +0000

    batching tests

---
+++ batch.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -x
+
+set -e
+
+wc_prepare() {
+    for scale in {20..36}; do
+        SCALE=$scale ./workloads/wordcount/prepare/prepare.sh
+    done
+}
+
+pr_prepare() {
+    for scale in {21..25}; do
+        SCALE=$scale ./workloads/pagerank/prepare/prepare.sh
+    done
+}
+
+ts_prepare() {
+    for scale in {31..36}; do
+        SCALE=$scale ./workloads/terasort/prepare/prepare.sh
+    done
+}
+
+wc_spark() {
+    for scale in {20..34..2}; do
+        for run in {1..3}; do
+            SCALE=$scale RUN=$run ./workloads/wordcount/spark/java/bin/run.sh
+        done
+    done
+    for scale in {21..33..2}; do
+        for run in {1..3}; do
+            SCALE=$scale RUN=$run ./workloads/wordcount/spark/scala/bin/run.sh
+        done
+    done
+}
+
+wc_flink() {
+    for scale in {20..34..2}; do
+        for run in {1..3}; do
+            SCALE=$scale RUN=$run ./workloads/wordcount/flink/java/bin/run.sh
+        done
+    done
+    for scale in {21..33..2}; do
+        for run in {1..3}; do
+            SCALE=$scale RUN=$run ./workloads/wordcount/flink/scala/bin/run.sh
+        done
+    done
+}
+
+for p in $@; do
+    $p
+done
